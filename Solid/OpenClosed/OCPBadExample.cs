cc4a312727018abc0275e2b6da8f9f4d22a29593
Kiarikita <isilzulalguner@gmail.com>
2022-06-01 13:44:51 +0300

    OpenClosed Principle done.

--- Solid/OpenClosed/OCPBadExample.cs
+++ Solid/OpenClosed/OCPBadExample.cs
@@ -8,5 +8,29 @@ namespace Solid.OpenClosed
 {
     public class OCPBadExample
     {
+        public void CollectFruits(string fruit, int quantity)
+        {
+            //Toplanan meyve miktarını gösteren bir uygulama olsun
+            if (fruit == "Apple")
+            {
+                //Eğer meyve Apple ise bize miktarı versin
+                Console.WriteLine("Toplanan elma sayısı: " + quantity);
+            }
+
+            if (fruit == "Pear")
+            {
+                //Eğer meyve Pear ise bize miktarı versin
+                Console.WriteLine("Toplanan armut sayısı: " + quantity);
+            }
+
+            if (fruit == "Orange")
+            {
+                //Eğer meyve Orange ise bize miktarı versin
+                Console.WriteLine("Toplanan portakal sayısı: " + quantity);
+            }
+
+            /*Bu kötü örnekte eğer kullanıcı başka bir meyve eklemek isterse yani 
+             * işlevselliğini geliştirmek istersek kaynağı değiştirmek zorunda kalacağız*/
+        }
     }
 }
--- Solid/OpenClosed/OCPGoodExample.cs
+++ Solid/OpenClosed/OCPGoodExample.cs
@@ -6,18 +6,36 @@ using System.Threading.Tasks;
 
 namespace Solid.OpenClosed
 {
+
+    /*Buradaki iyi örnekte eğer yeni bir meyve türünün eklenmesini istersek tüm metodu değiştirmemize gerek kalmadan,
+     yeni bir class oluşturum bu classın interfaceten kalıtım almasını sağlamak yeterli olacak*/
+
     public interface Fruit
     {
-
+        void CollectFruits(int quantity);
     }
-
-    public class Apple : Fruit
+    
+    public class Apple : Fruit //Apple sınıfı Fruit interfacesinden kaltım alarak CollectFruits methodunu kullandı
     {
-
+        public void CollectFruits(int quantity)
+        {
+            Console.WriteLine("Toplanan elma sayısı: " + quantity);
+        }
     }
 
     public class Pear : Fruit
     {
+        public void CollectFruits(int quantity)
+        {
+            Console.WriteLine("Toplanan armut sayısı: " + quantity);
+        }
+    }
 
+    public class Orange : Fruit
+    {
+        public void CollectFruits(int quantity)
+        {
+            Console.WriteLine("Toplanan portakal sayısı: " + quantity);
+        }
     }
 }
