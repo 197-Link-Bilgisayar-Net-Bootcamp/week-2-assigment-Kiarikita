a2f420498cdf0f86c3f0242bbfd8a29a2f8e5dd7
Kiarikita <isilzulalguner@gmail.com>
2022-06-01 15:22:14 +0300

    InterfaceSegretion Principle done.

---
+++ Solid/InterfaceSegregation/ISPBadExample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.InterfaceSegregation
+{
+    public interface IProducts
+    {
+        /*Ürünlerimizin sahip olacağı özellikleri tanımladık*/
+        int Id { get; set; }
+        int Stock { get; set; }
+        int Size { get; set; }
+        double Price { get; set; }
+        int LegLenght { get; set; }
+    }
+
+    public class Pant : IProduct
+    {
+        /* Pantolon ürünü için pantolon classında interfacete tanımladığımız özellikleri implemente ettik*/
+        public int Id { get; set; }
+        public int Stock { get; set; }
+        public int Size { get; set; }
+        public double Price { get; set; }
+        public int LegLenght { get; set; }
+    }
+
+    public class Orange : IProduct
+    {
+        /*Ancak eğer portakal ürününü eklemek istersek;
+         * portakalın bacak boyu ya da bedeni gibi özellikleri olmamasına rağmen
+         * bu özellikleri interface de tanımladığımız için bunları portakal sınıfı implement etmek zorunda kaldı.*/
+        public int Id { get; set; }
+        public int Stock { get; set; }
+        public int Size { get; set; }
+        public double Price { get; set; }
+        public int LegLenght { get; set; }
+    }
+
+    
+}
---
+++ Solid/InterfaceSegregation/ISPGoodExample.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.InterfaceSegregation
+{
+    public interface IProduct
+    {
+        //Ürünlerimizin sahip olacağı ortak özellikleri tanımladık
+        int Id { get; set; }
+        int Stock { get; set; } 
+        double Price { get; set; }
+        
+    }
+
+    public interface ITrouser
+    {
+        //Pantolon ürününün sahip olacağı spesifik özellikleri tanımladık
+        public int Size { get; set; }
+        public int LegLenght { get; set; }
+    }
+
+    public interface IApple
+    {
+        //Elma ürününün sahip olacağı spesifik özellikleri tanımladık
+        public string AppleType { get; set; }
+    }
+
+    public class Trouser : IProduct, ITrouser
+    {
+        public int Id { get; set; }
+        public int Stock { get; set; }
+        public double Price { get; set; }
+        public int Size { get; set; }
+        public int LegLenght { get; set; }
+    }
+
+    public class Apple : IProduct, IApple
+    {
+        public int Id { get; set; }
+        public int Stock { get; set; }
+        public double Price { get; set; }
+        public string AppleType { get; set; }
+    }
+
+    //prensibi uygulayarak her sınıf ortak ürün özelliklerini ürün interfaceinden, spesifik özelliklerini ise kendi interfacelerinden almış oldu
+
+
+}
